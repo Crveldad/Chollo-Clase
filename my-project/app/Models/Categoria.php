ded05f2b99a42c47e0cbc0cba761f8739a445cf3
Crveldad <crveldad@hotmail.com>
2022-02-04 15:58:51 +0100

    modelo categorias

---
+++ my-project/app/Models/Categoria.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
+
+class Categoria extends Model
+{
+    public $timestamps = false;
+    use HasFactory;
+
+    /*public function chollos(){
+        //esto no está del todo bien, hay que poner la PK y la FK
+        return $this->hasMany(Chollo::class);
+    }*/
+}
